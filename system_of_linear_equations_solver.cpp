e2c7f4fe2a62d19bc184d0f7dacafed99822aaf0
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-04-09 23:48:51 +0300

    modified @ Thu  9 Apr 23:48:07 EEST 2020

--- system_of_linear_equations_solver.cpp
+++ system_of_linear_equations_solver.cpp
@@ -17,30 +17,7 @@ int main(){
         cin >> d[rows];
     }
 
-
-	/*double eq1[3], eq2[3], eq3[3];
-    double d1, d2, d3;
-    for ( int lol = 0 ; lol < 4 ; lol++ ){
-        cout << (char)(97 + lol) << "1 = ";
-        cin >> (lol != 3 ? eq1[lol] : d1);
-    }
-    for ( int lol = 0 ; lol < 4 ; lol++ ){
-        cout << (char)(97 + lol) << "1 = ";
-        cin >> (lol != 3 ? eq2[lol] : d2);
-    }
-    for ( int lol = 0 ; lol < 4 ; lol++ ){
-        cout << (char)(97 + lol) << "1 = ";
-        cin >> (lol != 3 ? eq3[lol] : d3);
-    }
-
-/*I was checking STH
-    printf("d1 = %lf, d2 = %lf, d3 = %lf \n",d1,d2,d3);
-
-    for ( int lol = 0 ; lol < 3 ; lol++ ){
-        printf("eq1[%d] = %lf, eq2[%d] = %lf ,eq2[%d] = %lf\n",lol,eq1[lol],lol,eq2[lol],lol,eq3[lol]);
-    }
-*/
-    /*Two variables boys
+/*  Two variables boiz :)
     //some headstarts a1 = eq1[0] , b2 = eq2[1] and so on ....
     //D = ( eq1[0]*eq2[1] ) - ( eq2[0]*eq1[1] );
     //Dx = ( eq1[2]*eq2[1] ) - ( eq2[2]*eq1[1] );

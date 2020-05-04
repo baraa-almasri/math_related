8f88b87bb89124f59af84c204c7293189a7761c0
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-05-04 12:29:30 +0300

    modified @ StarWars Day

--- polygon_generator.cpp
+++ polygon_generator.cpp
@@ -4,6 +4,8 @@
 #include <utility>
 #include <vector>
 #define pi 3.1415902991527593
+//reminder    x = r*cos(theta), y = r*sin(theta)
+//hence    theta = arccos(x/r) or arcsin(y/r)
 double genXcartesian(double radius,double angle){
     return radius*cos(angle);
 }
@@ -11,10 +13,9 @@ double genYcartesian(double radius,double angle){
     return radius*sin(angle);
 }
 int main(){
-    // reminder    x = radius*cos(theta), y = radius*sin(theta)
-    // hence    theta = arccos(x/radius) or arcsin(y/radius)
+
     double radius;
-    printf("Enter the radius of the circumference circle: ");
+    printf("Enter the radius of the circumferting circle: ");
     scanf("%lf", &radius);
     printf("Enter the cartesian coordinates of polygon's center (x,y): ");
     
@@ -25,29 +26,36 @@ int main(){
     printf("Enter number of sides of the polygon: ");
     scanf("%d",&noOfSides);
 
-    //from radius = S/2sin(pi/n)
-    //we got S = radius*2sin(pi/n)
+    //from r = S/2sin(pi/n)    |  S is the side length
+    //we got S = r*2sin(pi/n)  |    for future confusion
     double side;
     side = radius*2*sin(pi/noOfSides);
 
     //the angle between two neighbouring diameters of the polygon in radians
     double angle;
     angle = (pi*2)/noOfSides;
-    double theta = angle;
+    double theta = angle; //we'll need them later, just wait....
 
     printf("\n------------------------");
     printf("\nSide length = %lf\n",side);
 
     printf("------------------------\n");
     std::vector< std::pair<double,double> > points(noOfSides+1);
+
+    //calculating points' polar coordinates (compared to the center of the polygon),
+    //then converting them into catesian coordinates(compared to the center of the polygon),
+    //and adding the coordinates of the center.
+    //then voila we got the coordinates of the vertices(heads) 🎉🎉
     
     for(int point = 1 ; point <= noOfSides ; point++){
         points.at(point) = std::make_pair(x + genXcartesian(radius, theta), y + genYcartesian(radius, theta));
         printf("Point %d (%lf , %lf)\n", point, points.at(point).first, points.at(point).second);
         theta += angle;
-    }
- 
+    } //also I started indexes with one so the first is the origin (0,0)
+
+
     //Points with index-1
+    //to calculate the slopes duh.😐
     std::vector< std::pair<double,double> > prevPoints(noOfSides+1);
     std::vector< std::pair<double,double> > differences(noOfSides+1);
 
@@ -56,33 +64,36 @@ int main(){
         differences.at(point).first = points.at(point).first - prevPoints.at(point).first;
         differences.at(point).second = points.at(point).second - prevPoints.at(point).second;
     }
+    //difference between the first and the last point so it's useful for their line
     differences.at(1).first = points.at(1).first - points.back().first;
     differences.at(1).second = points.at(1).second - points.back().second;
 
     printf("\n------------------------\n");
 
+    //finally the slopes and lines equation generation
     double slope;
     for(int point = 1 ; point <= noOfSides ; point++){
-        //printf("diff X %d = %lf \n",point ,differences.at(point).first );
-        //printf("diff Y %d = %lf \n",point ,differences.at(point).second );
 
+        //for vertical slopes since you can't devide by zero 🙃
 		if(differences.at(point).first == 0){
             printf("\nSide %d Equation: ",point);
-            printf("y = %lf", points.at(point).first);
+            printf("y%d = %lf",point, points.at(point).first);
         }
+        //for normal slopes that doesn't involve deviding by zero 🎉
 		else{
             slope = differences.at(point).second / differences.at(point).first;
             printf("\nSide %d Equation: ",point);
-            printf("y = %lf * (x - %lf) + %lf", slope,  points.at(point).first , points.at(point).second);
+            printf("y%d = %lf * (x - %lf) + %lf",point, slope,  points.at(point).first , points.at(point).second);
         }
 
     }
 
+    //last but not least, I liked to add it, so that every juicable thing is here 👽👽
     printf("\n\n------------------------\n\n");
     printf("Circumferting circle's equation:\n");
     printf("(x - %lf)^2 + (y - %lf)^2 = %lf \n",x ,y, pow(radius,2));
 
-    //Mafs LOL
+    //Mafs LOL 🤓🤓
     
     return 0;
 }

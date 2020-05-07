5de7c2c81e90ac276f4475586b0dbdb326003dae
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-05-07 14:54:22 +0300

    re-rectified @ Thu  7 May 14:53:54 EEST 2020

--- newtonRootFinder.py
+++ newtonRootFinder.py
@@ -21,19 +21,17 @@ def diff(point, approachingPoint, fx):
     # f'(a) = (f(a+h) - f(a))/(h)
     return (func(point + approachingPoint, fx) - func(point, fx)) / approachingPoint
 
-
 def findRootnewton(initialPoint, fx, error):
     difference = 9e12  # assigned to a very high value to make the loop's condition true
     xi = initialPoint  # first point
     xi1 = 0  # i-th point
     steps = 0
-    while error <= difference or difference != 0:
+    while error <= difference:
         # reminder of newton's method formula:
         # x0 = constant //initial point
         # Xi+1 = Xi - (f(Xi)/f'(Xi)) //i-th point
         xi1 = xi - (func(xi, fx) / diff(xi, 0.0000001, fx))
         difference = abs(xi1 - xi)
-
         steps += 1
         xi = xi1
 

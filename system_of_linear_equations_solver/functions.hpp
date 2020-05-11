9c9573e84c1f6f0aefe4a3b5141df681d68ddea3
baraa-almasri <baraa.masri@asu.edu.jo>
2020-05-11 18:50:30 +0300

    modified @

--- system_of_linear_equations_solver/functions.hpp
+++ system_of_linear_equations_solver/functions.hpp
@@ -1,30 +1,54 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout << "System of the form a1X + b1Y + c1Z = d1 , a2X + b2Y .... and so on\n"
-		<< "Enter the coefficents\n"
-		<< "AND DON'T ENTER THE SAME NUMBERS for both of the equations!!\n";
+#include <iostream>
+#include <stdio.h>
+
+void twoUnknowns(){
+    printf("System of the form a1X + b1Y = c1 , a2X + b2Y .... and so on, sorry I'm board\n");
+	printf("Enter the coefficents\n"); 
+	printf("AND DON'T ENTER THE SAME NUMBERS for both of the equations!!\n");
+	
+    double coefficients[2][2];
+    double c[2];
+    
+    for(int rows(0) ; rows < 2 ; rows++){
+        for(int cols(0) ; cols < 2 ; cols++){
+            printf("%c%i = ", (char)(97 + cols), rows+1);
+            std::cin >> coefficients[rows][cols];
+        }
+        printf("c%i = ", rows+1);
+        std::cin >> c[rows];
+    }
+
+    //Using Carmer's rule....
+    double D, Dx, Dy, Dz;
+
+    D = (coefficients[0][0]*coefficients[1][1]) - (coefficients[1][0]*coefficients[0][1]);
+    Dx = (c[0]*coefficients[1][1]) - (c[1]*coefficients[0][1]);
+    Dy = (coefficients[0][0]*c[1]) - (c[0]*coefficients[1][0]);
+    
+    printf("X = %lf\n", Dx/D);
+    printf("Y = %lf\n", Dy/D);
+    
+}
+void threeUnknowns(){
+
+    printf("System of the form a1X + b1Y + c1Z = d1 , a2X + b2Y .... and so on\n");
+	printf("Enter the coefficents\n"); 
+	printf("AND DON'T ENTER THE SAME NUMBERS for both of the equations!!\n");
 	
     double coefficients[3][3];
     double d[3];
     
     for(int rows(0) ; rows < 3 ; rows++){
         for(int cols(0) ; cols < 3 ; cols++){
-            cout << (char)(97 + cols) << rows+1 <<" = ";
-            cin >> coefficients[rows][cols];
+            printf("%c%i = ", (char)(97 + cols), rows+1);
+            std::cin >> coefficients[rows][cols];
         }
-        cout << "d" << rows+1 << " = ";
-        cin >> d[rows];
+        printf("d%i = ", rows+1);
+        std::cin >> d[rows];
     }
 
-/*  Two variables boiz :)
-    //some headstarts a1 = eq1[0] , b2 = eq2[1] and so on ....
-    //D = ( eq1[0]*eq2[1] ) - ( eq2[0]*eq1[1] );
-    //Dx = ( eq1[2]*eq2[1] ) - ( eq2[2]*eq1[1] );
-    //Dy = ( eq1[0]*eq2[2] ) - ( eq1[2]*eq2[0]);
-*/
-
     //Using Carmer's rule....
+
     double D, Dx, Dy, Dz;
     
     D = (coefficients[0][0]*coefficients[1][1]*coefficients[2][2])
@@ -54,10 +78,10 @@ int main(){
         -(coefficients[0][0]*d[1]*coefficients[2][1])
         -(coefficients[0][1]*coefficients[1][0]*d[2])
         -(d[0]*coefficients[1][1]*coefficients[2][0]);
-            
-    cout << "X = " << (Dx/D) << endl;
-    cout << "Y = " << (Dy/D) << endl;
-    cout << "Z = " << (Dz/D) << endl;
 
-    return 0;
+    printf("X = %lf\n", Dx/D);
+    printf("Y = %lf\n", Dy/D);
+    printf("Z = %lf\n", Dz/D);
+
 }
+

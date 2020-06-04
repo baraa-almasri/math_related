27f48b5696943c52f0da6dc9d77dc0967f690f6e
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-06-04 20:50:51 +0300

    refactored @ Thu  4 Jun 20:50:51 EEST 2020

--- System of Linear Equations Solver/functions.hpp
+++ System of Linear Equations Solver/functions.hpp
@@ -4,12 +4,11 @@
 #include <iostream>
 using namespace std;
 
-void readMatrix(int rows, vector<vector<double> >& matrix){
-    int cols = rows; // for readablity
-    for(int row = 0 ; row < rows ; row++){
+void readMatrix( vector<vector<double> >& matrix, int order ){
+    for(int row = 0 ; row < order ; row++){
         // temporary vector to store columns
         vector<double> tempVector;
-        for(int col = 0 ; col < cols ; col++){
+        for(int col = 0 ; col < order ; col++){
             // temporary variable to store element
             double temp;
             printf("a%i%i = ", row+1, col+1);
@@ -21,16 +20,18 @@ void readMatrix(int rows, vector<vector<double> >& matrix){
         // add the temporary vector to the current row of the main 2D vector
         matrix.push_back(tempVector);   
         // and roll over if condition is true :) 
+    	printf("\n");
     }
 }
-/* return cofactor of an element of a matrix */
-vector< vector<double> > getCofactor( vector< vector<double> > matrix, int mainMatrixSize, int excludedRow, int excludedColumn){
+
+/* return cofactor matrix of an element of a matrix */
+vector< vector<double> > getCofactor( vector< vector<double> > matrix, int order, int excludedRow, int excludedColumn){
 	/* coofactored matrix  */
 	vector< vector<double> > newMatrix;
 
-	for( int row = 0; row < mainMatrixSize; row++ ){
+	for( int row = 0; row < order; row++ ){
 		vector< double > temp;
-		for( int col = 0; col < mainMatrixSize; col++ ){
+		for( int col = 0; col < order; col++ ){
 			/* if element in the main matrix doesn't intersect with the 
 			 *  excluded row or column then it's added to the cofactor matrix */
 			if( row != excludedRow && col != excludedColumn){
@@ -47,21 +48,20 @@ vector< vector<double> > getCofactor( vector< vector<double> > matrix, int mainM
 }
 
 // the actual determinant finder :)
-double det(int rows, std::vector<std::vector<double>> mainMatrix){ //wondering why called mainMatrix
-    int cols = rows; // for readablity                 //scroll down to see some messed up blin, will be changed :)
+double det( vector<vector<double>> matrix, int order ){ 
     double answer = 0;
-    if(rows == 2){
-        return ( mainMatrix[0][0] * mainMatrix[1][1] ) - ( mainMatrix[1][0] * mainMatrix[0][1] );
+    if( order == 2 ){
+        return ( matrix[0][0] * matrix[1][1] ) - ( matrix[1][0] * matrix[0][1] );
     }
 
-    // first row, alternating columns
-    for(int mainCol = 0; mainCol < cols; mainCol++){
+    // first row alternating columns
+    for(int mainCol = 0; mainCol < order; mainCol++){
         vector< vector<double> > newMatrix;
 	/* equaling the new matrix with the cofactor of each element of the first row
 	 * of the main matrix
 	 */
-        newMatrix = getCofactor( mainMatrix , rows, 0, mainCol);
-	answer += pow(-1, mainCol) * mainMatrix[0][mainCol] * (det(rows - 1, newMatrix));
+        newMatrix = getCofactor( matrix , order, 0, mainCol);
+	answer += pow(-1, mainCol) * matrix[0][mainCol] * (det( newMatrix, order - 1 ));
     }
 
     // finally return the answer when the recieved matrix is 1*1
@@ -69,89 +69,48 @@ double det(int rows, std::vector<std::vector<double>> mainMatrix){ //wondering w
 
 }
 
-/* the actual determinant finder :)/
-double det(int rows, std::vector<std::vector<double>> mainMatrix){ //wondering why called mainMatrix
-    int cols = rows; // for readablity                 //scroll down to see some messed up blin 
-    double answer = 0;
-    if(rows == 2){
-        return ( mainMatrix[0][0] * mainMatrix[1][1] ) - ( mainMatrix[1][0] * mainMatrix[0][1] );
-    }
-
-    int newRows, newCols;
-    newRows = rows - 0; // those two where "rows - 1" then I realised that
-    newCols = cols - 0; // this moo ain't working so....
-
-
-    // first row, alternating columns
-    for(int mainCol = 0; mainCol < cols; mainCol++){
-        std::vector< std::vector<double> > newMatrix;
-        // for future reads :)
-        // I had to stop @ newRows - 1 so the index won't be out of range in the main matrix
-        for(int newRow = 0; newRow < newRows - 1; newRow++){
-            // temporary vector to store columns
-            std::vector<double> tempVector;
-                
-            for(int newCol = 0; newCol < newCols; newCol++){
-                    
-                if( newCol != mainCol ){
-                    double temp;
-                    temp = mainMatrix[newRow+1][newCol];
-                    tempVector.push_back(temp);
-
-                }
-            }
-            // add the temporary vector to the current row of the main 2D vector
-            newMatrix.push_back(tempVector);
-
-        }
-        answer += pow(-1, mainCol) * mainMatrix[0][mainCol] * (det(newRows-1, newMatrix));
-    }
-
-    // finally return the answer when the recieved matrix is 1*1
-    return answer;
-
-}
-*/
+/* find solution of system of n linear equations */
 void solveSystem(int order){
-    printf("System of the form a1X1 + a2X2 + .... + anXn = b\n");
-	printf("Enter the coefficents\n"); 
+	puts("System of the form a1X1 + a2X2 + .... + anXn = b\n");
+	puts("Enter the coefficents\n"); 
 	// the augmented matrix, well part of it	
-    vector<vector<double>> matrix;
-	readMatrix(order, matrix);
+	vector<vector<double>> matrix;
+	readMatrix( matrix, order );
   	
+	puts("enter constants:\n");
   	// constants vector(math vector not that STL)
-    double constants[order];
-    for(int moo = 0; moo < order; moo++){
-        printf("b%i = ", moo+1);
-        scanf("%lf", &constants[moo]);
-    }
+	double constants[order];
+    	for(int moo = 0; moo < order; moo++){
+        	printf("b%i = ", moo+1);
+		scanf("%lf", &constants[moo]);
+    	}
 
-    //Using Carmer's rule....
-    double D;
-    D = det(order, matrix); 
+	//Using Carmer's rule....
+	double D;
+	D = det( matrix, order ); 
 
-    // since it's a system for n variables we're gonna need a vector to store those variables
-    vector<double> rawVariables; // you'll find why it's called raw :) in line 
-    // but first we need matrices with the constant column so....
-    vector<vector<double>> tempMatrix;
-    tempMatrix = matrix;
-    for(int column = 0; column < order; column++){
-        // now swinging between rows
-        for(int row = 0; row < order; row++){
-            tempMatrix[row][column] = constants[row];   
-        }
-        rawVariables.push_back( det(order, tempMatrix) );
+	// since it's a system for n variables we're gonna need a vector to store those variables
+	vector<double> rawVariables; // you'll find why it's called raw :) in line 
+	// but first we need matrices with the constant column so....
+	vector<vector<double>> tempMatrix;
+	tempMatrix = matrix;
+	for(int column = 0; column < order; column++){
+		// now swinging between rows
+		for(int row = 0; row < order; row++){
+        		tempMatrix[row][column] = constants[row];   
+    		}
+		rawVariables.push_back( det( tempMatrix, order ) );
 
-        tempMatrix = matrix;
-    }
-    // k because I couldn't find a better name, you got a better name suggest it I'm all ears
-    for(int k = 0; k < order; k++){
-        printf("var%i = %lf \n",k+1 , rawVariables[k]/D);
-    }
-  
-  /*for(int k : rawVariables){
-        printf("var = %lf \n" , k/D);
-    }
-  */
- 
+		tempMatrix = matrix;
+	}
+	// k because I couldn't find a better name, you got a better name suggest it I'm all ears
+	for(int k = 0; k < order; k++){
+		printf("var%i = %lf \n",k+1 , rawVariables[k]/D);
+	}
+
+	 /*for(int k : rawVariables){
+	       printf("var = %lf \n" , k/D);
+	   }
+	 */
+
 }
--- System of Linear Equations Solver/main.cpp
+++ System of Linear Equations Solver/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "functions.hpp"
 int main(){
-    printf("Enter order of unknowns: ");
+    printf("Enter number of unknowns: ");
     int unknowns;
     scanf("%i", &unknowns);
     solveSystem(unknowns);

98de934a6cdd17a7a40f332e48f6a707a776c4f4
Baraa Al-Masri <hexagon16rpm@yandex.com>
2020-10-04 15:05:13 +0300

    ....

--- ProjectFunction/Function.h
+++ ProjectFunction/Function.h
@@ -1,139 +1,92 @@
-#include <string> // for solvePostfix function
-#include <math.h> // for execOperator function
-#include <stdlib.h> // used malloc only
-#include <stack> // for convertInToPost function
-#include <ctype.h> // for convertInToPost function
-#include <regex> // regular expression
+#include <string> // lol
+#include <math.h> // pow
+#include <stdlib.h> // malloc 
+#include <regex> // regex_replace
+
+using std::string;
 
 class Function {
 public:
-    Function(std::string equation) {
-        this->equation = equation;
+    Function(string polynomial) {
+        this->polynomial = polynomial;
+
+    }
+
+    ~Function() {
+
     }
     
-    int evaluate(int value) {
-        // temp string 
-        std::string tmp;
-        // convert function equation to postfix
-        tmp = convertInToPost(this->equation);
-        // regex to match x
-        std::regex x("[x]");
 
-        // get the evaluated value
-        return solvePostfix(std::regex_replace(tmp, x, std::to_string(value)) );
+    double at(double point) {
+
+        return this->evaluateInfix(this->polynomial);
     }
 
 
 private:
 
-    std::string equation;
+    string polynomial;
 
-    // functions
+private: // functions
+    
+    double evaluateInfix(string polynomial) {
+
+    	for ( int k = 0; k < polynomial.size(); k++ ) {
+            string sFirstOperand{""};
+            string sSecondOperand{""};
+
+      		if ( isOperator(polynomial[k]) ) {
+                char op = polynomial[k];
+                // to replace operators with hash to replace it with the value
+                polynomial[k] = '#'; 
+
+                for( int l = k-1; !isOperator(polynomial[l]) && l >= 0; l-- ) {
+                    sFirstOperand.push_back(polynomial[l]);
+                }
+
+                std::reverse(sFirstOperand.begin(), sFirstOperand.end());   
+
+                for( int l = k+1; !isOperator(polynomial[l]) && l < polynomial.size(); l++ ) {
+                    sSecondOperand.push_back(polynomial[l]);
+                    // new index
+                    k = l;
+                }
+            
+                sFirstOperand.push_back('#');
+                sFirstOperand += sSecondOperand;
+                sFirstOperand.push_back('*');
+
+                polynomial = std::regex_replace(polynomial, 
+                    (std::regex)sFirstOperand, std::to_string( 
+                    execOperator( stod(sFirstOperand)
+                        , stod(sSecondOperand)
+                        , op) )
+                );
+    		}
+    	}		
+
+        return std::stod(polynomial);
+    }
+
+    string replaceX(double x) {
+
+    }
 
     // execute a binary operator using a character variable
-    static int execOperator(int rightOperand, int leftOperand, char operation){
-        switch(operation){
-            case '+':
-                return rightOperand + leftOperand;
-            case '-':
-                return rightOperand - leftOperand;
-            case '*':
-                return rightOperand * leftOperand;
-            case '/':
-                return rightOperand / leftOperand;
-            case '^':
-                return pow(rightOperand,  leftOperand);
-            default: 
-                printf("Wrong operation!!\n");
-        }
+    static double execOperator(double rightOperand, double leftOperand, char op){
+
+        return (op == '+')? rightOperand + leftOperand:
+            (op == '-')? rightOperand - leftOperand:
+            (op == '*')? rightOperand * leftOperand:
+            (op == '/')? rightOperand / leftOperand: 
+            (op == '^')? pow(rightOperand, leftOperand): 0;
     }
 
     // check for operator
     static bool isOperator(char character){
         char c = character;
-        return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') ? 1 : 0;
+
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
     }
-
-    // convert infix to postfix
-    static std::string convertInToPost(std::string infix) {
-        std::stack<char> *tmp = new std::stack<char>;
-        
-        std::string postfix = "";
-        char t;
-        
-        for(int i = 0; i < infix.length() ;i++) {   
-            
-            switch(infix[i]) {
-                case '^':
-                case '-': 
-                case '+':
-                case '/':
-                case '*':
-                tmp->push(infix[i]);
-                break;
-                case ')':   
-                    postfix.push_back(tmp->top());
-                    tmp->pop();
-                    break;
-            }
-            
-            if(isalnum(infix.at(i))) {
-                postfix.push_back(infix.at(i));
-            }
-        }
-        
-        return postfix;
-    }
-
-    // solve postfix (duh)
-    static int solvePostfix(std::string postfix){
-        for(int lol = 0; lol < postfix.length(); lol++){
-            // temporary string to store the original string after evaluating two numbers with thier operator
-            std::string temp = "";
-            if( isOperator(postfix[lol]) ){
-                // step one: append elements before the operands & operator to the temporary string
-
-                // i starts @ 0 because the main string will have final results of operands & operators
-                // & ends @ main index - 2 because the index has the operator & the two indexes before it has the operands
-                for(int i = 0; i < lol - 2; i++){
-                    if(i >= 0)
-                        temp.push_back(postfix.at(i));
-                }
-                // step two: append the evaluated number to the temporary string
-
-                int rightOperand = (int)(postfix[lol-2] - 48);
-                int leftOperand = (int)(postfix[lol-1] - 48);
-
-                int result = execOperator( rightOperand, leftOperand, postfix[lol] );
-                
-                temp.push_back( (char)(result + 48) );
-
-                // step three: 
-                // append the rest of the postfix into the temp string
-
-                for(int i = lol + 1; i < postfix.length(); i++){
-                    temp.push_back(postfix.at(i));
-                }
-
-                // step four
-                // update the postfix string with the evaluated answers
-
-                postfix = temp;
-
-                // step five
-                // reset the counter to match with the new postfix string
-
-                lol = 0;
-
-                // step six if there's no more operators return the final number
-                
-                if( postfix.length() == 1){
-                    return std::stoi(postfix);
-                }
-
-            }
-
-        }
-    }
-
+    
 };
--- ProjectFunction/driver.cpp
+++ ProjectFunction/driver.cpp
@@ -1,98 +1,12 @@
-//#include "Function.h"
-#include <stdio.h>
+#include "Function.h"
 #include <string>
-#include <regex>
-#include <ctype.h>
-#include <iostream>
-#include <algorithm>
-#include <math.h>
-
-using std::string;
-using std::regex;
-
-bool isOperator(char character){
-    char c = character;
-
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
-}
-
-double execOperator(double rightOperand, double leftOperand, char operation){
-    switch(operation){
-        case '+':
-            return rightOperand + leftOperand;
-        case '-':
-            return rightOperand - leftOperand;
-        case '*':
-            return rightOperand * leftOperand;
-        case '/':
-            return rightOperand / leftOperand;
-        case '^':
-            return pow(rightOperand,  leftOperand);
-        default: 
-            printf("Wrong operation!!\n");
-            return 0;
-    }
-}
-using std::stod;
-double evalFromString(string polynomial) {
-
-	for ( int k = 0; k < polynomial.size(); k++ ) {
-        string sFirstOperand{""};
-        string sSecondOperand{""};
-
-		if ( isOperator(polynomial[k]) ) {
-            char op = polynomial[k];
-            // to replace operators with hash to replace it with the value
-            polynomial[k] = '#'; 
-
-            for( int l = k-1; !isOperator(polynomial[l]) && l >= 0; l-- ) {
-                sFirstOperand.push_back(polynomial[l]);
-            }
-
-            std::reverse(sFirstOperand.begin(), sFirstOperand.end());   
-
-            for( int l = k+1; !isOperator(polynomial[l]) && l < polynomial.size(); l++ ) {
-                sSecondOperand.push_back(polynomial[l]);
-                // new index
-                k = l;
-            }
-            
-            sFirstOperand.push_back('#');
-            sFirstOperand += sSecondOperand;
-            sFirstOperand.push_back('*');
-
-            polynomial = std::regex_replace(polynomial, 
-                (std::regex)sFirstOperand, std::to_string( 
-                execOperator( stod(sFirstOperand)
-                    , stod(sSecondOperand)
-                    , op)
-                )
-            );
-                    
-
-		}
-	
-	}		
-
-    return std::stod(polynomial);
-}
+#include <stdio.h>
 
 
 int main() {
+    Function fn("2.1-3.14*10");
 
-    string fun = "2*6+33";
+    printf("val: %lf\n", fn.at(9));
 
-    std::cout << evalFromString(fun) << std::endl;
-
-    std::regex x("16#2*");
-    string lol = "18";
-
-    fun = std::regex_replace(fun, x, lol);
-
-    std::cout << fun << std::endl;
-
-    //Function *f = new Function("(x+3)");
-//    printf("f(5) = %d\n", f->evaluate(6));
-
-     return 0;
+    return 0;
 }
